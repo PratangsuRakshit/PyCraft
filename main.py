ede15dc54c64acd80b9e2dc8aab6e309ea51abeb
SAKETplayZzz <88712327+SAKETplayZzz@users.noreply.github.com>
2021-08-12 22:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,128 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 from perlin_noise import PerlinNoise
 from classes import *
+# Please NOTE : inv1 = hotbar inventory and inv2 = inventory overall 
+class BG(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui,
+            model = 'quad',
+            scale = (0.465,0.065),
+            position = Vec2(0,-0.465),
+            color = color.rgba(0, 0, 0.75,100),
+            )
+class Underlay(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui,
+            model = 'quad',
+            scale = (0.46,0.06),
+            position = Vec2(0,-0.465),
+            color = color.rgba(0, 0, 0.75,100),
+            texture = load_texture('Minecraft-in-Python-main/assets/inventory underlay.png'))
 
+class Inv1(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui,
+            scale = (0.05,0.05),
+            model = 'quad',
+            position = Vec2(-0.2,-0.465),
+            texture = load_texture('Minecraft-in-Python-main/assets/grassinv1.png')
+        )
+
+    def input(self, key):
+        if key == "1":
+            self.scale = 0.06
+        else:
+            self.scale = 0.05
+
+
+class Inv2(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            scale=(0.05, 0.05),
+            model='quad',
+            position= Vec2(-0.15, -0.465),
+            texture=load_texture('Minecraft-in-Python-main/assets/stoneinv1.png')
+            )
+
+    def input(self, key):
+        if key == "2":
+            self.scale = 0.06
+        else:
+            self.scale = 0.05
+
+class Inv3(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            scale=(0.05, 0.05),
+            model='quad',
+            position= Vec2(-0.10, -0.465),
+            texture=load_texture('Minecraft-in-Python-main/assets/cobbleinv1.png')
+            )
+
+    def input(self, key):
+        if key == "3":
+            self.scale = 0.06
+        else:
+            self.scale = 0.05
+
+class Inv4(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            scale=(0.05, 0.05),
+            model='quad',
+            position= Vec2(-0.05, -0.465),
+            texture=load_texture('Minecraft-in-Python-main/assets/dirtinv1.png')
+            )
+
+    def input(self, key):
+        if key == "4":
+            self.scale = 0.06
+        else:
+            self.scale = 0.05
+class Inv5(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            scale=(0.05, 0.05),
+            model='quad',
+            position= Vec2(0, -0.465),
+            texture=load_texture('Minecraft-in-Python-main/assets/loginv1.png')
+            )
+
+    def input(self, key):
+        if key == "5":
+            self.scale = 0.06
+        else:
+            self.scale = 0.05
+class Inv6(Entity):
+    def __init__(self):
+        super().__init__(
+            parent=camera.ui,
+            scale=(0.05, 0.05),
+            model='quad',
+            position= Vec2(0, -0.465),
+            texture=load_texture('Minecraft-in-Python-main/assets/leafinv1.png')
+            )
+    def input(self, key):
+        if key == "6":
+            self.scale = 0.06
+        else:
+            self.scale = 0.05
 app = Ursina()
+bg = BG()
+underlay = Underlay()
+inv1 = Inv1()
+inv2 = Inv2()
+inv3 = Inv3()
+inv4 = Inv4()
+inv5 = Inv5()
+inv6 = Inv6()
 log_texture = load_texture('assets/log_texture.png')
 grass_texture = load_texture('assets/grass_block.png')
 stone_texture = load_texture('assets/stone_block.png')
